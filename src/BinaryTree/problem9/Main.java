6c49d6c2e89e926e502cf919c149f1bb79c79298
Prateek Verma <prateek222rithik@gmail.com>
2025-07-29 22:25:29 +0530

    dfs debug

--- src/BinaryTree/problem9/Main.java
+++ src/BinaryTree/problem9/Main.java
@@ -18,19 +18,29 @@ class TreeNode {
 
 // Diameter
 class Solution {
-    int diameter = 0; //common variable for height and diameterOfBinaryTree
+    int diameter = 0;
     public int diameterOfBinaryTree(TreeNode root) {
         height(root);
-        return diameter-1;
+        return diameter;
     }
-    public int height(TreeNode node){
-        if(node==null) return 0;
+    public int height(TreeNode node) {
+        if (node == null) return 0;
         int left = height(node.left);
+        //firstly goes to the leftmost leaf(11) that will return height=0, left =0;
         int right = height(node.right);
-        int dia = left + right + 1;
-        diameter = Math.max(dia,diameter);
-        return Math.max(left,right)+1;
-    }
+        // jab return 0 hua from left then check it's right also null therefore, right=0
+        diameter = Math.max(diameter, left + right); // (0,0+0) -> 0
+        return Math.max(left, right) + 1;   // height(11) = 0+1 =1
+    } // left - right - node = post order traversal
+    // ab return hoga to aage ka check mt karna values jo mile usse leke piche jao
+    // after 11 goes back to 9's right i.e. null therefore return 0
+    // left of 9(11) gives 1 and right of 9(null) gives 0
+    // therefore diameter till 9 = (0,1+0) = 1 and height of 9= (1,0)+1 = 2;
+    // now, goes back to right of 3 (proceed post order wise(hint)) -> goes till last of left
+    // (15) -> left=0,right=0 (both nulls) -> diameter = (2,0)=2 (not of 15 but ab tak ka highest store kr rakha h)
+    // (0,0)+1=1 -> height(15)=1 -> goes to 7 -> same return 1 -> dia(till20) = (2,2)=2
+    // height(20)= (1,1)+1=2 returns 2 -> Now 3 -> left=2, right=2 -> dia=(2,2+2)=4,height(3)=(2,2)+1=3
+    // Stack empties -> final diameter = 4 ans
 }
 public class Main {
     public static void main(String[] args) {
