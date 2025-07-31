16d991b1018196c5806708c69bf08dd13c29ccdf
Prateek Verma <prateek222rithik@gmail.com>
2025-07-31 12:25:19 +0530

    two pointer

---
+++ src/Arrays/TwoPointer/problem3/Solution.java
@@ -0,0 +1,31 @@
+package Arrays.TwoPointer.problem3;
+// Leetcode - 88
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i=0,j=0,p=0;
+        int[] ans = new int[m+n];
+        while(i<m && j<n) {
+            if(p<m+n) {
+                if(nums1[i]<=nums2[j]) {
+                    ans[p++]=nums1[i++];
+                } else {
+                    ans[p++] = nums2[j++];
+                }
+            }
+        }
+        if(i==m) {
+            while(j!=n) {
+                ans[p++] = nums2[j++];
+
+            }
+        }
+        else if (j==n) {
+            while(i!=m) {
+                ans[p++] = nums1[i++];
+            }
+        }
+        for (int k = 0; k < m + n; k++) {
+            nums1[k] = ans[k];
+        }
+    }
+}
---
+++ src/Arrays/TwoPointer/problem4/Main.java
@@ -0,0 +1,35 @@
+package Arrays.TwoPointer.problem4;
+
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        left[0] = height[0];
+        for(int i=1;i<n;i++){
+            left[i] = Math.max(left[i-1],height[i]);
+        }
+
+        right[n-1] = height[n-1];
+        for(int i=n-2;i>=0;i--){
+            right[i] = Math.max(right[i+1],height[i]);
+        }
+        int sum =0;
+        for(int i=0;i<n;i++){
+            sum+= (Math.min(left[i],right[i])-height[i]);
+        }
+        return sum;
+    }
+}
+
+
+public class Main
+{
+    public static void main(String[] args) {
+        int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
+        Solution sol = new Solution();
+        int ans = sol.trap(height);
+        //print result
+        System.out.println(ans);
+    }
+}
